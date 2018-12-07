be1895d294830e979af49fc1cf8ff1b993029804
rkuus <32262605+rkuus@users.noreply.github.com>
2018-12-07 09:13:06 +0100

    Startlxc file
    Sets up the containers and stuff

---
+++ startlxc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PARSED_OPTIONS=$(getopt -o abcd --long start,begin,stop,help -- "$@")
+
+eval set -- "$PARSED_OPTIONS"
+
+while true; do
+        case "$1" in
+                -a|--start)
+                        sudo systemctl stop lighttpd
+                        lxc-start -n u1
+                        lxc-start -n u2
+                        sleep 10
+                        shift
+                        break;;
+                -b|--begin)
+                        sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 10.0.3.11
+                        shift
+                        break;;
+                -c|--stop)
+                        sudo iptables -t nat -D PREROUTING 1
+                        lxc-stop -n u1
+                        lxc-stop -n u2
+                        shift
+                        break;;
+                -d|--help)
+                        echo "Usage: sysinfo [OPTIONS...]"
+                        echo -e "\n --start start webserver"
+                        echo -e " --stop stop webserver"
+                        shift
+                        break;;
+                --)
+                        shift
+                        break;;
+        esac
+done
+
+exit 0
